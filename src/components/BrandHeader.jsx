93089b68ba3ec5dff3efb9f394faed4cfd99158a
skacjswo1234 <9078807@naver.com>
2025-10-30 13:50:06 +0900

    수정

--- src/components/BrandHeader.jsx
+++ src/components/BrandHeader.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 export default function BrandHeader() {
   return (
     <div className="flex items-center gap-2 select-none">
-      <div className="h-6 flex items-center">
+      <div className="h-8 md:h-9 flex items-center">
         {/* Light mode logo */}
-        <img src="/images/logo.png" alt="브랜디업" className="h-6 block dark:hidden" />
+        <img src="/images/logo.png" alt="브랜디업" className="h-8 md:h-9 block dark:hidden" />
         {/* Dark mode logo */}
-        <img src="/images/logo_dark.png" alt="브랜디업" className="h-6 hidden dark:block" />
+        <img src="/images/logo_dark.png" alt="브랜디업" className="h-8 md:h-9 hidden dark:block" />
       </div>
     </div>
   )
